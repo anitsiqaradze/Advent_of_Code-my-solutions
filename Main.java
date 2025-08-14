40092e796ea5a4e1d774dca2ffd0620bcdb25455
anitsiqaradze <93982931+anitsiqaradze@users.noreply.github.com>
2025-08-15 01:09:03 +0400

    Day 1 Part Two

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileNotFoundException;
 
 
@@ -9,24 +7,37 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-          int sum =0;
-          ArrayList<Integer> leftList = new ArrayList<>();
-          ArrayList<Integer> RightList = new ArrayList<>();
+
+        HashMap<Integer, Integer> uniqueNumbers = new HashMap<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
         try {
-            Scanner scanner = new Scanner(new File("input.txt"));
 
-           while (scanner.hasNextLine()) {
-              String line = scanner.nextLine();
-               divide(leftList,RightList, line);
-          }
-           Collections.sort(leftList);
-           Collections.sort(RightList);
+            Scanner scanner = new Scanner(new File("input1.txt"));
+            while(scanner.hasNextLine()) {
+                String[] parts = scanner.nextLine().trim().split("\\s+");
+                uniqueNumbers.put(Integer.parseInt(parts[0]), 0);
+                numbers.add(Integer.parseInt(parts[1]));
+            }
 
-           for(int i = 0; i < leftList.size(); i++) {
-               sum+=RightList.get(i)>leftList.get(i)?RightList.get(i)-leftList.get(i):leftList.get(i)-RightList.get(i);
-           }
-           System.out.println(sum);
+
+            for(int i = 0; i <  numbers.size(); i++) {
+                for(int j : uniqueNumbers.keySet()) {
+                    if(numbers.get(i) == j) {
+                          uniqueNumbers.put(j, uniqueNumbers.get(j) + 1);
+                    }
+                }
+            }
+
+            int result = 0;
+            for(int j : uniqueNumbers.keySet()) {
+                result += uniqueNumbers.get(j)*j;
+            }
+
+            System.out.println(result);
+
+
+
             scanner.close();
        }catch(FileNotFoundException e) {
        System.err.println("file not found" + e.getMessage());
@@ -34,14 +45,14 @@ public class Main {
 
     }
 
-    public static void divide(ArrayList<Integer> left, ArrayList<Integer> right, String Line) {
-
-       String[] parts = Line.trim().split("\\s+");
-       left.add(Integer.parseInt(parts[0]));
-       right.add(Integer.parseInt(parts[1]));
-
-
-    }
+//    public static void divide(ArrayList<Integer> left, ArrayList<Integer> right, String Line) {
+//
+//       String[] parts = Line.trim().split("\\s+");
+//       left.add(Integer.parseInt(parts[0]));
+//       right.add(Integer.parseInt(parts[1]));
+//
+//
+//    }
 
 
 
